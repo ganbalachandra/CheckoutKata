b9ae7ce9686a29bc32b7464de6aec585a107542b
gan <gan.suranga@outlook.com>
2017-10-03 14:48:17 +0100

    2nd test added and still RED

--- CheckoutKata/Checkout.Test/CheckoutTest.cs
+++ CheckoutKata/Checkout.Test/CheckoutTest.cs
@@ -12,6 +12,7 @@ namespace Checkout.Test
     {
         /// <summary>
         /// start with simplest test and make it fail (red) then make it just enough to pass
+        /// BDD style syntax is used
         /// </summary>
         public class When_scanned_no_items
         {
@@ -28,6 +29,20 @@ namespace Checkout.Test
                 Assert.That(totalPrice, Is.EqualTo(0));
 
             }
+        }
+
+        [TestFixture] //2nd checkout
+        public class When_scanned_oneItem_and_A
+        {
+            [Test]
+            public void Then_total_is_50()
+            {
+                var checkout = new Checkout();
+                var totalPrice = checkout.GetTotalPrice();
+                //assert
+                Assert.That(totalPrice, Is.EqualTo(50));
+
+           }
         }
     }
 }
